61acc165923e91cd0cf2c4080520b0843052d9f5
Wellington Guedes <juniozguedes@gmail.com>
2021-04-25 19:27:42 -0300

    config files

---
+++ src/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  aws_table_name: "movies-dynamodb-nodejs",
+  aws_local_config: {
+    region: "local",
+    endpoint: "http://localhost:8000",
+  },
+  aws_remote_config: {
+    accessKeyId: "AKIATNWZNIZS6QPE45FP",
+    secretAccessKey: "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",
+    region: "sa-east-1",
+  },
+};
